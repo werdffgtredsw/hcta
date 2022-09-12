d037e7a34cc8c18bc606a983c9167748053da63d
girutbe <104609044+girutbe@users.noreply.github.com>
2022-09-12 02:09:12 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=c4b4da47-494a-4d47-9587-17f7e4aeb771
+AUUID=94518814-7816-4b29-8421-6ad7e424ff10
 CADDYIndexPage=https://github.com/Externalizable/bongo.cat/archive/master.zip
 CONFIGCADDY=/etc/Caddyfile
 CONFIGXRAY=/etc/xray.json
